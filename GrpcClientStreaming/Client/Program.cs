ac074593ba61085824e989edc16f98503035efa3
Vishvaka <vishvaka@gmail.com>
2021-08-26 00:00:26 +0530

    gRPC Client Streaming

---
+++ GrpcClientStreaming/Client/Program.cs
@@ -0,0 +1,68 @@
+﻿using Grpc.Net.Client;
+using ProtoBuf;
+using ProtoBuf.Grpc;
+using ProtoBuf.Grpc.Client;
+using ProtoBuf.Grpc.Configuration;
+using System.Runtime.CompilerServices;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+var fileService = channel.CreateGrpcService<IFileService>();
+
+Console.WriteLine("Uploading File");
+
+var startTime = DateTime.Now;
+
+var result = await fileService.UploadFileAsync(CreateFileChunksAsync(@"D:\music.mp3"));
+
+var duration = DateTime.Now - startTime;
+
+Console.WriteLine($"Duration: {duration.TotalSeconds} seconds");
+
+Console.ReadLine();
+
+static async IAsyncEnumerable<FileChunk> CreateFileChunksAsync(
+    string filePath,
+    [EnumeratorCancellation] CancellationToken cancellationToken = default)
+{
+    using var file = File.OpenRead(filePath);
+    var buffer = new byte[8192];
+
+    while (true)
+    {
+        var read = await file.ReadAsync(buffer, cancellationToken);
+
+        if (read == 0)
+        {
+            break;
+        }
+
+        yield return new FileChunk
+        {
+            Data = buffer,
+        };
+    }
+}
+
+[Service]
+public interface IFileService
+{
+    [Operation]
+    Task<FileUploadResult> UploadFileAsync(
+        IAsyncEnumerable<FileChunk> fileChunks,
+        CallContext context = default);
+}
+
+[ProtoContract]
+public class FileChunk
+{
+    [ProtoMember(1)]
+    public byte[] Data { get; init; } = Array.Empty<byte>();
+}
+
+[ProtoContract]
+public class FileUploadResult
+{
+    [ProtoMember(1)]
+    public bool Success { get; init; }
+}
---
+++ GrpcClientStreaming/Server/Program.cs
@@ -0,0 +1,77 @@
+using ProtoBuf;
+using ProtoBuf.Grpc.Configuration;
+using ProtoBuf.Grpc.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCodeFirstGrpc();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<FileService>();
+
+    endpoints.MapGet("/", () => "Hello from File Service!");
+});
+
+app.Run();
+
+public class FileService : IFileService
+{
+    private readonly ILogger<FileService> _logger;
+
+    public FileService(ILogger<FileService> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<FileUploadResult> UploadFileAsync(
+        IAsyncEnumerable<FileChunk> fileChunks,
+        CancellationToken cancellationToken = default)
+    {
+        // Saving the file to temp folder for demo
+        var path = Path.GetTempFileName();
+
+        using (var file = File.OpenWrite(path))
+        {
+            await foreach (var fileChunk in fileChunks)
+            {
+                await file.WriteAsync(fileChunk.Data, cancellationToken);
+            }
+
+            await file.FlushAsync(cancellationToken);
+        }
+
+        _logger.LogInformation("Saved uploaded file at: {path}", path);
+
+        return new FileUploadResult
+        {
+            Success = true,
+        };
+    }
+}
+
+[Service]
+public interface IFileService
+{
+    [Operation]
+    Task<FileUploadResult> UploadFileAsync(
+        IAsyncEnumerable<FileChunk> fileChunks,
+        CancellationToken cancellationToken = default);
+}
+
+[ProtoContract]
+public class FileChunk
+{
+    [ProtoMember(1)]
+    public byte[] Data { get; init; } = Array.Empty<byte>();
+}
+
+[ProtoContract]
+public class FileUploadResult
+{
+    [ProtoMember(1)]
+    public bool Success { get; init; }
+}
